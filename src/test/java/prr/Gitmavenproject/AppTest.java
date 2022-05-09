f86142d450c9f660f12c79f90d85abd0ab258fcb
AmarKedar <amarkedar2012@gmail.com>
2022-05-09 10:09:56 +0530

    First commit

---
+++ src/test/java/prr/Gitmavenproject/AppTest.java
@@ -0,0 +1,20 @@
+package prr.Gitmavenproject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
